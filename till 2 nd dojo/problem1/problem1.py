1a9ef5a106965c2b4949bf1387494a2f5bded829
Aditya sharma <aditya2007aaditya@gmail.com>
2026-08-15 00:53:53 +0530

    soln

---
+++ till 2 nd dojo/problem1/problem1.py
@@ -0,0 +1,24 @@
+"""
+in this problem i clearly dont know what to do at first
+i am going to learn what that block do in the end then 
+that libraray what it does  
+"""
+""" so what i found is that
+
+
+import math
+
+
+
+distance = math.dist([x1, y1], [x2, y2]) 
+
+
+
+this particular calculates the distance between two points in a 2D space
+"""
+""" also we have to return that value after our defined function 
+
+"""
+import math
+distance = math.dist([x1, y1], [x2, t2]) 
+"this the core logic then "
