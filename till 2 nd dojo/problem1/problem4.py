3074b22f040c6f88bfdc127313bdeff5112dfa2c
Aditya sharma <aditya2007aaditya@gmail.com>
2026-08-17 10:19:19 +0530

    soln

---
+++ till 2 nd dojo/problem1/problem4.py
@@ -0,0 +1,17 @@
+""" 
+some concepts u came to know
+input function creates user input to a string by itself 
+we can acceses the string by using the variable name and this print(x.[whichever no yoou enter])
+
+
+
+ a second concept is that we can directly covert the 
+ i will do with a example using a map function to convert the string to 
+ integer and then we can use it in our code
+
+ a, b = map(int, input("enter the no of terms: ").split())
+ a, b = map(float, input().split())
+
+"""
+x = input()
+print(x[0])
