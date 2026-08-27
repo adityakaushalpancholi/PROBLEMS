260c3efaaac20efaa4eac85f6a2e90d1f4dfe739
Aditya sharma <aditya2007aaditya@gmail.com>
2026-08-28 00:58:41 +0530

    adi

--- worksheet/6.py
+++ worksheet/6.py
@@ -7,13 +7,38 @@ reversed() .
 Hint:	Build	a	new	empty	string,	and	either	loop	through	the	original	string	backwards	using	indices,	or	loop
 forwards	and	prepend	each	character	to	the	result	instead	of	appending
 """
-x = input( " Enter a string : ")
+
+"""
+x = int(input( " Enter a string : "))
 
 string = []
 while x!=0:
     v = x%10
     x = x//10
-    v = int(v)
     string += v 
-
 print(string)
+so here my approach is right but logic fails 
+""" 
+
+
+
+
+
+"""
+string = input()
+reversed_string = ""
+for i in range(1, len(string) + 1):
+    reversed_string += string[-i]
+print(reversed_string)
+
+"""
+
+x = int(input("Enter a number: "))
+
+reversed_num = 0
+while x != 0:
+    digit = x % 10
+    reversed_num = reversed_num * 10 + digit
+    x = x // 10
+
+print(reversed_num) # here the fall back is we cannot take other input like alphabates 
