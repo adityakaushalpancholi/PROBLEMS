1e91e816f3696e239ad5507530543038ff673e74
Aditya sharma <aditya2007aaditya@gmail.com>
2026-08-27 16:16:49 +0530

    aditya

--- worksheet/3.py
+++ worksheet/3.py
@@ -1,13 +1,16 @@
 """
-input: 1 
+input: 5 
 
 
 output: 
-1
-1 2
-1 2 3
-1 2 3 4
-1 2 3 4 5
 
 
+Hint: Use an outer loop for each row (1 to n) and an 
+inner loop that runs from 1 up to the current row number.
 """
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+    
