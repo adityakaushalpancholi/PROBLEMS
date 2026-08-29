6010fa81e2d656bf591bfb302e72260de5e2c4d0
Aditya sharma <aditya2007aaditya@gmail.com>
2026-08-29 09:56:03 +0530

    adi

---
+++ work in python/worksheet/9.py
@@ -0,0 +1,12 @@
+"""
+Q9.	Second	Largest	Number	in	a	List	(Medium	—	Loops	+	Conditionals)
+Find	the	second	largest	number	in	a	list	using	a	single	loop	(no	sorting).
+Hint:	Track	two	variables	—	
+largest	and	
+second_largest	—	and	update	both	carefully	inside	the	loop.	Think
+through	what	happens	when	a	new	number	is	bigger	than	
+largest ,	versus	when	it’s	only	bigger	than	
+second_largest .
+
+
+"""
